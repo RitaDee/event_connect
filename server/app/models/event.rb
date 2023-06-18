ade01d965dc82886a315fb0067d621d66df6aa23
Lukman Abdulkarim <109611523+lukman155@users.noreply.github.com>
2023-06-18 12:21:41 +0100

    Add: Model Association and Validation

--- server/app/models/event.rb
+++ server/app/models/event.rb
@@ -1,4 +1,11 @@
 class Event < ApplicationRecord
   belongs_to :user
   belongs_to :venue
+
+  has_many :tickets
+
+  validates :title, presence: true
+  validates :date, presence: true
+  validates :time, presence: true
+  validates :description, presence: true
 end
--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :events
+  has_many :tickets
+
+  validates :name, presence: true
 end

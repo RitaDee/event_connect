2494c1f2b591b771b32a9cb8c19f5fe3f2c2e03b
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-06-20 08:36:03 +0100

    Update signout route

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,19 @@ Rails.application.routes.draw do
                    sessions: 'api/v1/users/sessions',
                    registrations: 'api/v1/users/registrations'
                  }
+
       resources :reservations
       resources :tickets
       resources :events
       resources :venues
-      resources :users
+      resources :users do
+        collection do
+          delete 'sessions', to: 'users/sessions#destroy'
+        end
+      end
     end
   end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

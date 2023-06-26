3fd01245f49008537717eb7074133b54157d3578
Lukman Abdulkarim <109611523+lukman155@users.noreply.github.com>
2023-06-26 12:12:53 +0100

    Add: Controller Functions

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :events
   has_many :tickets
+  has_many :reservations, dependent: :destroy
 
   validates :name, presence: true
 
